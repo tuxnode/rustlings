729b2a3358195ecdbbfa2d6a2d468d38b2b0e27d
tuxnode <tuxnode@outlook.com>
2026-08-14 14:14:37 +0800

    feat: finish 05 vec

--- exercises/05_vecs/vecs1.rs
+++ exercises/05_vecs/vecs1.rs
@@ -5,6 +5,10 @@ fn array_and_vec() -> ([i32; 4], Vec<i32>) {
     // Use the vector macro.
     // let v = ???;
 
+    let mut v = Vec::new();
+    for i in a {
+        v.push(i);
+    }
     (a, v)
 }
 
--- exercises/05_vecs/vecs2.rs
+++ exercises/05_vecs/vecs2.rs
@@ -4,6 +4,8 @@ fn vec_loop(input: &[i32]) -> Vec<i32> {
     for element in input {
         // TODO: Multiply each element in the `input` slice by 2 and push it to
         // the `output` vector.
+
+        output.push(*element * 2);
     }
 
     output
@@ -23,9 +25,7 @@ fn vec_map(input: &[i32]) -> Vec<i32> {
     // See the example in the function `vec_map_example` above.
     input
         .iter()
-        .map(|element| {
-            // ???
-        })
+        .map(|element| element * 2)
         .collect()
 }
 
